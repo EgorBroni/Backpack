f289aaff797df4be592a9b54c03dc6ae5cdf1264
EgorBroni <ebronitskiy123@gmail.com>
2022-04-20 20:00:10 +0300

    test_1

--- Backpack_v2.0/main.cpp
+++ Backpack_v2.0/main.cpp
@@ -6,10 +6,39 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    int x = 5;
+
+using namespace std;
+
+
+int main() {
+    int W, n;
+    cin >> n >> W;
+    
+    int inf = 100000000;
+    
+    vector <int> w(n + 1);
+    
+    for (int i = 1; i <= n; i++){
+        cin >> w[i];
+}
+    
+    vector < vector <int> > dp(n + 1, vector <int>(W + 1, -inf));
+    dp[0][0] = 0;
+    for (int i = 1; i <= n; i++){
+        for (int j = 0; j <= W; j++){
+            if (w[i] <= j){
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + w[i]); //+ c[i]
+            } else {
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    int ans = 0;
+    for (int w = 0; w <= W; w++){
+        ans = max(ans,dp[n][w]);
+    }
+    cout << ans << endl;
     return 0;
 }
