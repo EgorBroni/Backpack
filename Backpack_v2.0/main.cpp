87743cf7813953bd6511ce4aac83e98cadea15c5
EgorBroni <ebronitskiy123@gmail.com>
2022-04-20 11:51:38 +0300

    Initial Commit

---
+++ Backpack_v2.0/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Backpack_v2.0
+//
+//  Created by Егор Броницкий on 20.04.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
